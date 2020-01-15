a3fc88c750051df08a2b34e308ea6718d95ca1f1
Tiffany Siu <tsiu88@gmail.com>
2020-01-15 11:40:55 -0800

    try given example of mad lib

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#formOne").submit(function(){
+    var person1Input = $("#person1").val();
+    var person2Input = $("#person2").val();
+    var animalInput = $("#animal").val();
+    var exclamationInput = $("#exclamation").val();
+    var verbInput = $("#verb").val();
+    var nounInput = $("#noun").val();
+
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+
+  });
+});
