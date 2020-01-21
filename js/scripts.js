becb9403c4e18c52e8c5d564d3fe7c7992d3fa7a
Brevin Cronk and Tiffany Siu <tiffany@>
2020-01-21 10:00:56 -0800

    add loops and arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,34 @@
 $(document).ready(function(){
   $("#formOne").submit(function(){
-    var person1Input = $("#person1").val();
-    var person2Input = $("#person2").val();
-    var animalInput = $("#animal").val();
-    var exclamationInput = $("#exclamation").val();
-    var animalNameInput = $("#animalName").val();
-    var noun1Input = $("#noun1").val();
-    var noun2Input = $("#noun2").val();
-    var verbInput = $("#verb").val();
-    var noun3Input = $("#noun3").val();
-    var worldNameInput = $("#worldName").val();
-    var worldNameCapitalize = worldNameInput.charAt(0).toUpperCase() + worldNameInput.slice(1);
+    // var person1Input = $("#person1").val();
+    // var person2Input = $("#person2").val();
+    // var animalInput = $("#animal").val();
+    // var exclamationInput = $("#exclamation").val();
+    // var animalNameInput = $("#animalName").val();
+    // var noun1Input = $("#noun1").val();
+    // var noun2Input = $("#noun2").val();
+    // var verbInput = $("#verb").val();
+    // var noun3Input = $("#noun3").val();
+    // var worldNameInput = $("#worldName").val();
+    // var worldNameCapitalize = worldNameInput.charAt(0).toUpperCase() + worldNameInput.slice(1);
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".animalName").append(animalNameInput);
-    $(".noun1").append(noun1Input);
-    $(".noun2").append(noun2Input);
-    $(".verb").append(verbInput);
-    $(".noun3").append(noun3Input);
-    $(".worldName").append(worldNameCapitalize);
+    // $(".person1").append(person1Input);
+    // $(".person2").append(person2Input);
+    // $(".animal").append(animalInput);
+    // $(".exclamation").append(exclamationInput);
+    // $(".animalName").append(animalNameInput);
+    // $(".noun1").append(noun1Input);
+    // $(".noun2").append(noun2Input);
+    // $(".verb").append(verbInput);
+    // $(".noun3").append(noun3Input);
+    // $(".worldName").append(worldNameCapitalize);
+
+    var blanks = ["person1", "person2", "animal", "exclamation", "animalName", "noun1", "noun2", "verb", "noun3", "worldName"];
+
+    blanks.forEach(function(blank){
+      var userInput = $("#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
 
